ffc4e17ec5fb70a227565763d7b1e125fa61623e
gordonthree <gordonthree@gmail.com>
2022-07-29 03:44:14 -0400

    spi interrupt firing, but not receiving data yet

--- src/main.cpp
+++ src/main.cpp
@@ -9,54 +9,77 @@
 int i=0;
 volatile boolean received;
 
-volatile byte Slavereceived,Slavesend;
+volatile int Slavereceived, Slavesend;
 
 int buttonvalue;
 
 uint8_t ledX = 0;
 
+char SPI_SlaveReceive(void);
+
 // the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
+  // initialize LEDs as outputs
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+
+  // spi stuff
   pinMode(MISO, OUTPUT); 
   pinMode(SS, INPUT);
+  pinMode(MOSI, INPUT);
+  pinMode(SCK, INPUT); // LED_BUILTIN
   
   // enable spi slave mode
-  SPCR |= _BV(SPE); 
-
+  //SPCR |= _BV(SPE); 
+  
+  // from the Microchip datasheet page 140
+  SPCR = (1<<SPIE); // enable SPI interrupt
+  SPCR = (1<<SPE); // enable slave mode 
   
   SPI.attachInterrupt();   
 
-
   Serial.begin(115200);
 
+  Serial.println("Hello, world!");
+  
+  received = false;
 }
 
 // SPI Slave ISR
 ISR (SPI_STC_vect)
 {
-  Slavereceived = SPDR;                  
+  Slavereceived = SPI_SlaveReceive();                  
   received = true;                       
+}
+
+char SPI_SlaveReceive(void)
+{
+  /* Wait for reception complete */
+  while(!(SPSR & (1<<SPIF)))
+  ;
+  /* Return Data Register */
+  return SPDR;
 }
 
 // the loop function runs over and over again forever
 void loop() {
   i++;
 
-  if (Slavereceived==1){
-    digitalWrite(LED1, HIGH); //Sets pin as HIGH LED ON
-    //Serial.println("Slave LED ON");
+  if (received){
+    digitalWrite(LED2, HIGH); //Sets pin as HIGH LED ON
+    Serial.print("Slave recv: ");
+    Serial.println(Slavereceived);
+    received = false; // clear flag
   } else {
-    digitalWrite(LED1, LOW);     //Sets pin as LOW LED OFF
+    digitalWrite(LED2, LOW);     //Sets pin as LOW LED OFF
     //Serial.println("Slave LED OFF");
   }
 
   if (i>1000){
     i=0;
     ledX = ledX ^ 1; // xor previous state
-    digitalWrite(LED2, ledX);   // turn the LED on (HIGH is the voltage level)
+    digitalWrite(LED1, ledX);   // turn the LED on (HIGH is the voltage level)
     //Serial.println("Toggle LED");
   }
 
