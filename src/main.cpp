a09d0f7b401d3b2a7e96256051a0b04ffdde8b95
gordonthree <gordonthree@gmail.com>
2022-07-29 01:40:34 -0400

    adding spi slave code, and more LEDs for blinkin

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,64 @@
 #include <Arduino.h>
 #include <SPI.h>
 
-#define LED 13
+//SCK D13, MISO D12, MOSI D11, SS D10
+#define LED1 PD2
+#define LED2 PD3
+#define LED3 PD4
+
+int i=0;
+volatile boolean received;
+
+volatile byte Slavereceived,Slavesend;
+
+int buttonvalue;
+
+uint8_t ledX = 0;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED, OUTPUT);
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(MISO, OUTPUT); 
+  pinMode(SS, INPUT);
+  
+  // enable spi slave mode
+  SPCR |= _BV(SPE); 
+
+  
+  SPI.attachInterrupt();   
+
+
+  Serial.begin(115200);
+
+}
+
+// SPI Slave ISR
+ISR (SPI_STC_vect)
+{
+  Slavereceived = SPDR;                  
+  received = true;                       
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  i++;
+
+  if (Slavereceived==1){
+    digitalWrite(LED1, HIGH); //Sets pin as HIGH LED ON
+    //Serial.println("Slave LED ON");
+  } else {
+    digitalWrite(LED1, LOW);     //Sets pin as LOW LED OFF
+    //Serial.println("Slave LED OFF");
+  }
+
+  if (i>1000){
+    i=0;
+    ledX = ledX ^ 1; // xor previous state
+    digitalWrite(LED2, ledX);   // turn the LED on (HIGH is the voltage level)
+    //Serial.println("Toggle LED");
+  }
+
+  delay(1);
 }
