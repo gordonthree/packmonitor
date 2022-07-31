33791592bc01ea8e68431f868b0e923283a4d3d2
gordonthree <gordonthree@gmail.com>
2022-07-30 23:53:04 -0400

    switched to switch for event handling

--- src/main.cpp
+++ src/main.cpp
@@ -58,28 +58,110 @@ void receiveEvent(size_t howMany) {
   rxData.dataLen = howMany - 1;                                    // save the data length for future use
   Serial.printf("RX %u bytes\n", (uint8_t) rxData.dataLen);
   recvEvnt = true;                                                 // set event flag
-  if (rxData.cmdAddr==0x20) {
-    unsigned long timeStamp = atol(rxData.cmdData);
-    //Serial.printf("Command 0x20: Received timestamp %lu\n", timeStamp);
-    setTime(timeStamp);                                            // fingers crossed
-    mastersetTime = true;                                          // set flag
-  } else if (rxData.cmdAddr==0x30) {
-    digitalWrite(LED4, LOW);                                       // turn off LED4
-    //Serial.println("Command 0x30: LED 4 off");
-  } else if (rxData.cmdAddr==0x31) {
-    digitalWrite(LED4, HIGH);                     // turn on LED4
-    //Serial.println("Command 0x31: LED 4 on");
-  } else if (rxData.cmdAddr==0x32) {
-    Serial.print("Command 0x32: Received ");
-    i = rxData.dataLen;                   // length of string
-    while (x<i) {                                 // print string one char at a time
-      Serial.print((char) rxData.cmdData[x]);
-      x++;
-    }
-    Serial.println();                             // send a newline  
-  } else {                                        // unknown register
-    Serial.printf("Command 0x%X: Not recognized\n", rxData.cmdAddr);
-  }
+  switch  (rxData.cmdAddr) {
+    case 0x20: // set time from master
+      unsigned long timeStamp = atol(rxData.cmdData);
+      //Serial.printf("Command 0x20: Received timestamp %lu\n", timeStamp);
+      setTime(timeStamp);                                            // fingers crossed
+      mastersetTime = true;                                          // set flag
+    case 0x21:
+      uint16_t timeStamp = atol(rxData.cmdData);
+      //Serial.printf("Command 0x20: Received timestamp %lu\n", timeStamp);
+      setTime(timeStamp);                                            // fingers crossed
+      mastersetTime = true;                                          // set flag
+    case 0x22:
+    case 0x23:
+    case 0x24:
+    case 0x25:
+    case 0x26:
+    case 0x27:
+    case 0x28:
+    case 0x29:
+    case 0x2A:
+    case 0x2B:
+    case 0x2C:
+    case 0x2D:
+    case 0x2E:
+    case 0x2F:
+    case 0x30:
+      digitalWrite(LED4, LOW);                                       // turn off LED4
+      //Serial.println("Command 0x30: LED 4 off");
+    case 0x31:
+      digitalWrite(LED4, HIGH);                     // turn on LED4
+      //Serial.println("Command 0x31: LED 4 on");
+    case 0x32: 
+      Serial.print("Command 0x32: Received ");
+      i = rxData.dataLen;                   // length of string
+      while (x<i) {                                 // print string one char at a time
+        Serial.print((char) rxData.cmdData[x]);
+        x++;
+      }
+      Serial.println();                             // send a newline  
+    case 0x33:
+    case 0x34:
+    case 0x35:
+    case 0x36:
+    case 0x37:
+    case 0x38:
+    case 0x39:
+    case 0x3A:
+    case 0x3B:
+    case 0x3C:
+    case 0x3D:
+    case 0x3E:
+    case 0x3F:
+    case 0x40:
+    case 0x41:
+    case 0x42:
+    case 0x43:
+    case 0x44:
+    case 0x45:
+    case 0x46:
+    case 0x47:
+    case 0x48:
+    case 0x49:
+    case 0x4A:
+    case 0x4B:
+    case 0x4C:
+    case 0x4D:
+    case 0x4E:
+    case 0x4F:
+    case 0x50:
+    case 0x51:
+    case 0x52:
+    case 0x53:
+    case 0x54:
+    case 0x55:
+    case 0x56:
+    case 0x57:
+    case 0x58:
+    case 0x59:
+    case 0x5A:
+    case 0x5B:
+    case 0x5C:
+    case 0x5D:
+    case 0x5E:
+    case 0x5F:
+    case 0x60:
+    case 0x61:
+    case 0x62:
+    case 0x63:
+    case 0x64:
+    case 0x65:
+    case 0x66:
+    case 0x67:
+    case 0x68:
+    case 0x69:
+    case 0x6A:
+    case 0x6B:
+    case 0x6C:
+    case 0x6D:
+    case 0x6E:
+    case 0x6F:
+
+    default:// unknown register
+      Serial.printf("Command 0x%X: Not recognized\n", rxData.cmdAddr);
+  } // end switch
 }
 
 void setup() {
