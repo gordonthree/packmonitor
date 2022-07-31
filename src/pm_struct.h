5e619feb5105ce8a3f5970136a54d3792efc4c9b
gordonthree <gordonthree@gmail.com>
2022-07-31 17:29:25 -0400

    added struct for adc readings

--- src/pm_struct.h
+++ src/pm_struct.h
@@ -2,18 +2,26 @@
 
 const uint8_t txBufferSize = 50;
 const uint8_t rxBufferSize = 50;
+const uint8_t adcBufferSize = 3;        // adc1 current, adc2 pack voltage, adc3 bus voltage
+
 struct I2C_RX_DATA {
-  uint8_t cmdAddr = 0;                  // single byte command register
+  uint8_t cmdAddr               = 0;    // single byte command register
   uint8_t cmdData[rxBufferSize] = {};   // room for N bytes of data
-  char padding[10] = {};                // padding not sure it's needed
-  size_t dataLen = 0;
+  size_t  dataLen               = 0;    // number of bytes in buffer
 };
 
 struct I2C_TX_DATA {
   uint8_t cmdData[txBufferSize] = {};   // room for N bytes of data
-  char padding[10] = {};                // padding not sure it's needed
-  size_t dataLen = 0;
+  size_t  dataLen = 0;
 };
+
+struct ADC_DATA {
+  uint8_t adcPin   = 0;                 // Arduino pin number?
+  int32_t adcRaw   = 0;                 // raw value
+  float   adcFloat = 0.0;               // formatted value
+}
 
 volatile I2C_RX_DATA rxData;
 volatile I2C_TX_DATA txData;
+volatile adcDataBuffer[adcBufferSize];  // Enough room to store three adc readings
+          
