bb892fd222f82bb88febf6d99b922b9729b0f003
gordonthree <gordonthree@gmail.com>
2022-08-01 11:41:38 -0400

    Successfully compiles for atmega4809 now

--- src/pm_pins.h
+++ src/pm_pins.h
@@ -13,31 +13,28 @@
 #define ADC0 A0
 #define ADC1 A2
 #define ADC2 A2
-#elif MCU_NANOEVERY
-#define LED1 PF2
-#define LED2 PF3
-#define LED3 PF4
-#define LED4 PF5
-
-// Nano Every: SDA 4 SCL 5 
-#define SCL PA3 
-#define SDA PA2 
 #elif MCU_ATMEGA4808
 #define LED1 PF2
 #define LED2 PF3
 #define LED3 PF4
 #define LED4 PF5
 
+#define ADC0 A0
+#define ADC1 A2
+#define ADC2 A2
 // Nano Every: SDA 4 SCL 5 
 #define SCL PA3 
 #define SDA PA2 
 #elif MCU_ATMEGA4809
-#define LED1 PB2
-#define LED2 PC6
-#define LED3 PF5
-#define LED4 PA0
+#define LED1 5
+#define LED2 4
+#define LED3 3
+#define LED4 2
 
+#define ADC0 14
+#define ADC1 15
+#define ADC2 16
 // Nano Every: SDA 4 SCL 5 
-#define SCL PA3 
-#define SDA PA2 
+//#define SCL PA3 
+//#define SDA PA2 
 #endif
