40c0cb29b5bb06752cf38d7579144f6c67e08983
gordonthree <gordonthree@gmail.com>
2022-08-04 19:20:16 -0400

    updated pin assignments for DA

--- src/pm_pins.h
+++ src/pm_pins.h
@@ -1,68 +1,52 @@
 #include <Arduino.h>
 
-#ifdef MCU_ATMEGA328P
-#define LED1 PD2
-#define LED2 PD3
-#define LED3 PD4
-#define LED4 PD5
+#ifdef  MCU_AVR128DA28
+// LEDs used on breadboard only
+#define LED1 PIN_PA4 // PA0
+#define LED2 PIN_PA5 // PA1
+#define LED3 PIN_PA6 // PA2
+#define LED4 PIN_PA7 // PA3
 
-//SDA 18 (A4) SCL 19 (A5)
-#define SCL PC5 // A5
-#define SDA PC4 // A4
+// left hand side of the chip
+#define ADC0 PIN_PD1 // PD1
+#define ADC1 PIN_PD2 // PD2
+#define ADC2 PIN_PD3 // PD3
+#define ADC3 PIN_PD4 // PD4
 
-#define ADC0 A1
-#define ADC1 A2
-#define ADC2 A3
-#elif MCU_ATMEGA4808
-#define LED1 PF2
-#define LED2 PF3
-#define LED3 PF4
-#define LED4 PF5
+// Stand alone slave-only pins
+#define SCL  PIN_PA3 // Slave TWI0
+#define SDA  PIN_PA2 // " "
 
-#define ADC0 A0
-#define ADC1 A2
-#define ADC2 A2
-// Nano Every: SDA 4 SCL 5 
-#define SCL PA3 
-#define SDA PA2 
-#elif MCU_AVR128DA28
-#define LED1 7 // PA0
-#define LED2 8 // PA1
-#define LED3 9 // PA2
-#define LED4 10 // PA3
+// Dual-mode slave pins
+#define SCL0 PIN_PA3 // Slave TWI0
+#define SDA0 PIN_PA2 // " "
 
-#define ADC0 13 // PD1
-#define ADC1 14 // PD2
-#define ADC2 15 // PD3
-#define ADC3 16 // PD4
-// Nano Every: SDA 4 SCL 5 
-#define SCL 3 // PA3
-#define SDA 2 // PA2
-#elif MCU_AVR128DA32
-#define LED1 4 // PA0
-#define LED2 5 // PA1
-#define LED3 6 // PA2
-#define LED4 7 // PA3
+// Dual-mode master pins
+#define SCL1 PIN_PC3 // Master TWI0 
+#define SDA1 PIN_PC2 // " "
 
-#define ADC0 15 // PD1
-#define ADC1 16 // PD2
-#define ADC2 17 // PD3
-#define ADC3 18 // PD3
-// Nano Every: SDA 4 SCL 5 
-//#define SCL 3 // PA3
-//#define SDA 2 // PA2
-#elif MCU_NANOEVERY
-#define LED1 5 
-#define LED2 4
-#define LED3 3
-#define LED4 2
+#elif   MCU_AVR128DA32
+// LEDs used on breadboard only
+#define LED1 PIN_PA4
+#define LED2 PIN_PA5
+#define LED3 PIN_PA6
+#define LED4 PIN_PA7
 
-#define ADC0 15
-#define ADC1 16
-#define ADC2 17
-#define ADC3 18
+// bottom right of processor
+#define ADC0 PIN_PD3
+#define ADC1 PIN_PD4
+#define ADC2 PIN_PD5
+#define ADC3 PIN_PD6
 
-// Nano Every: SDA 4 SCL 5 
-#define SDA 18 
-#define SCL 19 
+// Stand alone slave-only pins
+#define SCL  PIN_PA3 // Slave TWI0
+#define SDA  PIN_PA2 // " "
+
+// Dual-mode slave pins
+#define SCL0 PIN_PA3 // Slave TWI0
+#define SDA0 PIN_PA2 // " "
+
+// Dual-mode master pins
+#define SCL1 PIN_PF3 // Master TWI1 
+#define SDA1 PIN_PF2 // " "
 #endif
