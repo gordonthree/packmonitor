c5f5aacf315280524f13d6af632a032ecba2c02d
gordonthree <gordonthree@gmail.com>
2022-08-04 16:43:51 -0400

    fixed pins for 128da28, added pins for 128da32

--- src/pm_pins.h
+++ src/pm_pins.h
@@ -26,14 +26,28 @@
 #define SCL PA3 
 #define SDA PA2 
 #elif MCU_AVR128DA28
-#define LED1 0 // PA0
-#define LED2 1 // PA1
-#define LED3 2 // PA2
-#define LED4 3 // PA3
+#define LED1 7 // PA0
+#define LED2 8 // PA1
+#define LED3 9 // PA2
+#define LED4 10 // PA3
 
 #define ADC0 13 // PD1
 #define ADC1 14 // PD2
 #define ADC2 15 // PD3
+#define ADC3 16 // PD4
+// Nano Every: SDA 4 SCL 5 
+#define SCL 3 // PA3
+#define SDA 2 // PA2
+#elif MCU_AVR128DA32
+#define LED1 4 // PA0
+#define LED2 5 // PA1
+#define LED3 6 // PA2
+#define LED4 7 // PA3
+
+#define ADC0 15 // PD1
+#define ADC1 16 // PD2
+#define ADC2 17 // PD3
+#define ADC3 18 // PD3
 // Nano Every: SDA 4 SCL 5 
 #define SCL 3 // PA3
 #define SDA 2 // PA2
@@ -46,6 +60,7 @@
 #define ADC0 15
 #define ADC1 16
 #define ADC2 17
+#define ADC3 18
 
 // Nano Every: SDA 4 SCL 5 
 #define SDA 18 
