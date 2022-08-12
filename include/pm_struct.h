32ff00c5ed05941c41793e27333c7e0ac4bbf74d
gordonthree <gordonthree@gmail.com>
2022-08-12 15:04:35 -0400

    created struct for temperature readings

--- include/pm_struct.h
+++ include/pm_struct.h
@@ -27,6 +27,13 @@ struct DEBUG_MSGS {
   char    messageTxt[50];
 };
 
+typedef struct tempsensor_t
+{
+  double  tempReading = 0.0;
+  double  lowReading  = 0.0;
+  double  highReading = 0.0;
+  uint8_t sensorAddr  = 0; 
+}
 union ulongArray
 {
   uint32_t longNumber=0;
