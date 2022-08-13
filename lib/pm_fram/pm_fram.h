7bc160724e3f96527dd05a06b5932b18b7809b51
gordonthree <gordonthree@gmail.com>
2022-08-13 14:01:08 -0400

    increase buffer size to 127 bytes

--- lib/pm_fram/pm_fram.h
+++ lib/pm_fram/pm_fram.h
@@ -29,7 +29,7 @@ class FRAMSTORAGE
     uint8_t byteArray[eedata_size];                          // byte array to send over i2c or store in fram
     } ;
 
-    static const uint16_t ee_buffer_size = 0x70;             // number of recordds in the buffer array
+    static const uint16_t ee_buffer_size = 0x7F;             // number of recordds in the buffer array
     static const uint16_t ee_record_size = sizeof(EERECORD); // calculate size of a record in bytes
     const uint16_t ee_start_offset       = 0x64;             // eeprom offset is 100 bytes (0x64), save that space for other uses
   
