70c2de72fe14f3059f27b593f8f00dbe84533b21
gordonthree <gordonthree@gmail.com>
2022-08-16 12:07:27 -0400

    removed unused fns

--- include/pm_struct.h
+++ include/pm_struct.h
@@ -4,16 +4,16 @@ const uint8_t txBufferSize = 50;
 const uint8_t rxBufferSize = 50;
 const uint8_t adcBufferSize = 4;        // adc1 current, adc2 pack voltage, adc3 bus voltage, adc4 coulumb counter
 
-struct I2C_RX_DATA {
-  uint8_t cmdAddr;                // single byte command register
-  uint8_t dataLen;                // number of bytes in buffer
-  uint8_t cmdData[rxBufferSize];  // room for N bytes of data
-};
+// struct I2C_RX_DATA {
+//   uint8_t cmdAddr;                // single byte command register
+//   uint8_t dataLen;                // number of bytes in buffer
+//   uint8_t cmdData[rxBufferSize];  // room for N bytes of data
+// };
 
-struct I2C_TX_DATA {
-  uint8_t dataLen;
-  uint8_t cmdData[txBufferSize];   // room for N bytes of data
-};
+// struct I2C_TX_DATA {
+//   uint8_t dataLen;
+//   uint8_t cmdData[txBufferSize];   // room for N bytes of data
+// };
 
 struct ADC_DATA {
   uint8_t adcPin   = 0;                 // Arduino pin number?
